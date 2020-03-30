7c0ece9c0e844ae3a1035e6852241ee8cf841212
Luka Pavicic <lukapavicic01@gmail.com>
2020-03-30 21:31:04 +0200

    send request to join vtc

--- src/screens/VtcScreen.jsx
+++ src/screens/VtcScreen.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {makeStyles} from "@material-ui/core/styles";
-import {Container, Paper, Typography, Grid, Card, Button} from "@material-ui/core";
+import {Container, Paper, Typography, Grid, Card, Button, TextField} from "@material-ui/core";
 import axios from 'axios';
 import {ROOT_API} from "../api_endpoint";
 import Navigation from "../components/Navigation";
@@ -9,6 +9,11 @@ import PeopleIcon from '@material-ui/icons/People';
 import DateRangeIcon from '@material-ui/icons/DateRange';
 import MarkunreadMailboxIcon from '@material-ui/icons/MarkunreadMailbox';
 import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import {useAuth} from "../store";
+import {Alert} from "@material-ui/lab";
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 const useStyles = makeStyles(theme => ({
     vtcCard: {
@@ -26,15 +31,49 @@ const useStyles = makeStyles(theme => ({
     generalInfoItem: {
         color: "#000000",
         marginTop: "5px",
-    }
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modalPaper: {
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        borderRadius: "5px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    inputLabel: {
+        "&$cssFocused": {
+            color: "green",
+        },
+    },
+    textOutlineInput: {
+        '&$cssFocused $notchedOutline': {
+            borderColor: `#27ae60 !important`,
+        }
+    },
+    cssFocused: {},
+    notchedOutline: {
+        borderWidth: '1px',
+    },
 }));
 
 export default function VtcScreen(props) {
 
     const classes = useStyles();
+    const {authToken, setAuthToken} = useAuth();
 
     const [currentVtc, setCurrentVtc] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    const [hasSentRequestToJoin, setHasSentRequestToJoin] = useState(false);
+    const [showJoinErrorAlert, setJoinErrorAlert] = useState(false);
+    const [sendRequestModalOpen, setSendRequestModalOpen] = useState(false);
+    const [motivationText, setMotivationText] = useState("");
+    const [hasRequestSucceeded, setHasRequestSucceeded] = useState(false);
 
     const _getCurrentVtc = () => {
         axios.get(`${ROOT_API}/v1/vtcs/${props.match.params.vtc_id}`).then(res => {
@@ -43,6 +82,44 @@ export default function VtcScreen(props) {
         }).catch(err => {
             console.log(err);
         })
+    };
+
+    const _showJoinAlert = (type) => {
+        // type 0 -> request failed
+        // type 1 -> request succeeded
+        switch (type) {
+            case 0:
+                setHasSentRequestToJoin(true);
+                setTimeout(() => setHasSentRequestToJoin(false), 10000);
+                break;
+            case 1:
+                setHasRequestSucceeded(true);
+                setTimeout(() => setHasRequestSucceeded(false), 10000);
+                break;
+        }
+    }
+
+    const _requestToJoin = () => {
+      axios.post(`${ROOT_API}/v1/join_request/new`, {
+          "join_request": {
+              "vtc_id": currentVtc.id,
+              "motivation_text": motivationText
+          }
+      }, {
+          headers: {
+              'Authorization': `Bearer ${authToken}`
+          }
+      }).then(res => {
+          setSendRequestModalOpen(false);
+          _showJoinAlert(1);
+          setMotivationText("");
+      }).catch(err => {
+          setMotivationText("");
+          setSendRequestModalOpen(false);
+          if(err.response.data.includes("User has already been taken")) {
+              _showJoinAlert(0);
+          }
+      })
     };
 
     useEffect(() => {
@@ -57,6 +134,50 @@ export default function VtcScreen(props) {
                 <div>
                     <Navigation vtcColor={currentVtc.main_color}/>
                     <Container style={{marginTop: "90px"}}>
+                        <Modal
+                            aria-labelledby="transition-modal-title"
+                            aria-describedby="transition-modal-description"
+                            className={classes.modal}
+                            open={sendRequestModalOpen}
+                            onClose={() => setSendRequestModalOpen(false)}
+                            closeAfterTransition
+                            BackdropComponent={Backdrop}
+                            BackdropProps={{
+                                timeout: 500,
+                            }}
+                        >
+                            <Fade in={sendRequestModalOpen}>
+                                <div className={classes.modalPaper}>
+                                    <Typography gutterBottom variant={"h6"}>Why should this VTC accept you?</Typography>
+                                    <Typography gutterBottom variant={"body1"} style={{color: "grey"}}>
+                                        This is a letter that will be seen by a VTC admin you are currently applying to. Write something nice about yourself.
+                                    </Typography>
+                                    <TextField
+                                        rows={5}
+                                        multiline
+                                        variant={"outlined"}
+                                        style={{width: "100%"}}
+                                        value={motivationText}
+                                        onChange={(e) => setMotivationText(e.target.value)}
+                                        placeholder={"Write something about you..."}
+                                        InputLabelProps={{
+                                            classes: {
+                                                root: classes.inputLabel,
+                                                focused: classes.cssFocused,
+                                            },
+                                        }}
+                                        InputProps={{
+                                            classes: {
+                                                root: classes.textOutlineInput,
+                                                focused: classes.cssFocused,
+                                                notchedOutline: classes.notchedOutline,
+                                            },
+                                        }}
+                                    />
+                                    <Button onClick={() => _requestToJoin()} variant={"contained"} style={{backgroundColor: currentVtc.main_color, color: "white", alignSelf: "center", marginTop: "10px"}}>REQUEST TO JOIN</Button>
+                                </div>
+                            </Fade>
+                        </Modal>
                         <Grid container>
                             <Grid item xs={12} sm={12} md={4} lg={4}>
                                 <Card className={classes.vtcCard}>
@@ -87,7 +208,7 @@ export default function VtcScreen(props) {
                                     </Typography>
                                 </Card>
                                 <Card className={classes.vtcCard} style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-                                    <Button variant={"contained"} style={{backgroundColor: currentVtc.main_color, color: "white", alignSelf: "center"}}>REQUEST TO JOIN</Button>
+                                    <Button onClick={() => setSendRequestModalOpen(true)} variant={"contained"} style={{backgroundColor: currentVtc.main_color, color: "white", alignSelf: "center"}}>REQUEST TO JOIN</Button>
                                 </Card>
                                 <Card className={classes.vtcCard}>
                                     <Typography gutterBottom style={{color: "grey"}}>Social Media Links</Typography>
@@ -105,6 +226,12 @@ export default function VtcScreen(props) {
                             </Grid>
                         </Grid>
                     </Container>
+                    {hasSentRequestToJoin ? <Alert onClose={() => setHasSentRequestToJoin(false)} severity={"error"} style={{position: "absolute", bottom: 5, left: 5}}>
+                        You already sent a request to join this VTC. Please wait for the response.
+                    </Alert> : null}
+                    {hasRequestSucceeded ? <Alert onClose={() => setHasRequestSucceeded(false)} severity={"success"} style={{position: "absolute", bottom: 5, left: 5}}>
+                        Request to join sent successfully. You will hear back from the VTC admin as soon as possible. Sit tight.
+                    </Alert> : null}
                 </div>
             }
         </div>
